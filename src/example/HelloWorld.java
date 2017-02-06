723657e95895156625ae06c61c03f6d0ca39eb53
Hanter Jung <hanterkr@gmail.com>
2017-02-06 09:58:43 +0900

    Initial Commit with JSP Project

---
+++ src/example/HelloWorld.java
@@ -0,0 +1,10 @@
+package example;/**
+ * Created by hanter on 2017. 2. 6..
+ */
+public class HelloWorld {
+  public String sayHelloWorldFrom(String from) {
+    String result = "Hello, world, from " + from;
+    System.out.println(result);
+    return result;
+  }
+}
